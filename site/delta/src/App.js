59dc0de142f2b4fda4a6a9796f306a8bd393fe6b
nelsasser <elsanic96@gmail.com>
2021-01-23 16:22:00 -0600

    LOGO! also added beginning of serverless backend

--- site/delta/src/App.js
+++ site/delta/src/App.js
@@ -1,38 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { ThemeProvider } from "styled-components";
-import { GlobalStyles } from "./components/globalStyles";
-import { lightTheme, darkTheme } from "./components/Themes";
-
 function App() {
-  const themeToggler = () => {
-    theme === 'light' ? setTheme('dark') : setTheme('light');
-  }
-
   return (
-    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-      <>
-      <GlobalStyles/>
-      <div className="App">
-        <button onClick={themeToggler}>Swith Theme</button>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-      </>
-    </ThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- site/delta/src/components/Toggler.js
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-import { func, string } from 'prop-types'
-import styled from "styled-components"
